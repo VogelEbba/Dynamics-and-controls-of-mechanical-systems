b6877c3d3530aa2ae468dd5aa59edab0d8ec4c6a
Floortje Stekelenburg <f.c.m.stekelenburg@student.tue.nl>
2025-12-10 22:22:37 +0100

    a and b

--- Deliverable_4.m
+++ Deliverable_4.m
@@ -1,1 +1,19 @@
-Gl = tf([5.1e-3 1000 250 0],[5e5 2.5e5 6.4e5 4.9e4 0]);
+clear all
+clc
+delete all
+syms s
+%% Computing the poles of the transfer function
+Gl = tf([5.1e-3 1000 250 ],[5e5 2.5e5 6.4e5 4.9e4 0]);
+H = tf([5.1e-3 1000 250 0 ],[5e5 2.5e5 6.4e5 4.9e4 0]);
+
+
+Hmin = minreal(H);      % this cancels common factors
+poles_Hmin = pole(Hmin)
+
+%% Generating impuls response function
+t = 0:0.01:91; 
+[y, tOut] = impulse(H,t);
+figure;
+plot(tOut, y);
+grid on
+xlim([0 91]);
