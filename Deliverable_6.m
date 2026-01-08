b9c7db81e1a44862d66fe07d449f6d59c7cc7fa6
Floortje Stekelenburg <f.c.m.stekelenburg@student.tue.nl>
2026-01-08 10:51:54 +0100

    klein foutje aangepast

--- Deliverable_6.m
+++ Deliverable_6.m
@@ -1,7 +1,7 @@
 clc; delete all; clear all;
 
 s = tf('s');
-dW = 100; %For question a at least
+dw = 100; %For question a at least
 G = (5.1e-3*s^2 + (0.2 + 0.049*dw)*s + 250)/ ... 
     (5e5*s^4 + (5e4 + 100*dw)*s^3 + 6.2e5*s^2 + 4.9e4*s);
 
